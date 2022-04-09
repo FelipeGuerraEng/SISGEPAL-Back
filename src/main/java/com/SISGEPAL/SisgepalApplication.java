5bdcd2fad147ce5042ebf0ec60ba4e621b91dce3
Dilan Joel Bergano Caicedo <=>
2022-04-09 18:27:45 -0500

    Primer commit

---
+++ src/main/java/com/SISGEPAL/SisgepalApplication.java
@@ -0,0 +1,13 @@
+package com.SISGEPAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SISGEPAL/SisgepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SISGEPAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
